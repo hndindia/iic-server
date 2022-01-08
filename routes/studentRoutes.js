1e1ac47413e5c16ade86186ba635fa343a4a3371
Shubham Jain <kumarshubham1008@gmail.com>
2022-01-08 18:37:49 +0530

    changed get to post

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -5,6 +5,6 @@ const {register, login} = require('../controller/studentController');
 const router = express.Router();
 
 router.post("/register", register);
-router.get("/login", login);
+router.post("/login", login);
 
 module.exports = router;
