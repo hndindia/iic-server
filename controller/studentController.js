f90dec656df477be6f212900651a5d7dcf05dc1e
karan siddhu <karansiddhu22@gmail.com>
2022-04-22 12:22:09 +0530

    Updated student model

--- controller/studentController.js
+++ controller/studentController.js
@@ -3,71 +3,71 @@ const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
-    try {
-        const user = await Student.create(req.body);
-        sendToken(user, 201, res);
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Email already exists",
-            error: error.message
-        });
-    }
+  try {
+    const user = await Student.create(req.body);
+    sendToken(user, 201, res);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Email already exists",
+      error: error.message
+    });
+  }
 };
 
 exports.login = async (req, res) => {
-    // console.log("🔥 -> ", req.body);
-    const { email, password } = req.body;
-    console.log("Email - ", email, "Password - ", password);
-    if (!email || !password) {
-        res.status(400).json({
-            success: false,
-            error: "The email and password field is required"
-        });
-        return;
+  // console.log("🔥 -> ", req.body);
+  const { email, password } = req.body;
+  console.log("Email - ", email, "Password - ", password);
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      error: "The email and password field is required"
+    });
+    return;
+  }
+
+  try {
+    const user = await Student.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "Student not found"
+      });
     }
 
-    try {
-        const user = await Student.findOne({ email }).select("+password");
-        if (!user) {
-            res.status(404).json({
-                success: false,
-                error: "Student not found"
-            });
-        }
+    const isMatch = await user.matchPasswords(password);
 
-        const isMatch = await user.matchPasswords(password);
-
-        if (!isMatch) {
-            res.status(404).json({
-                success: false,
-                error: "Invalid scredentials"
-            });
-        }
-
-        sendToken(user, 200, res);
-    } catch (error) {
-        console.log("ERROR - ", error);
-        res.status(500).json({
-            success: false,
-            message: "Please enter correct email and password",
-            error
-        });
+    if (!isMatch) {
+      return res.status(404).json({
+        success: false,
+        error: "Invalid scredentials"
+      });
     }
+
+    sendToken(user, 200, res);
+  } catch (error) {
+    console.log("ERROR - ", error);
+    res.status(500).json({
+      success: false,
+      message: "Please enter correct email and password",
+      error
+    });
+  }
 };
 
 exports.getUserById = (req, res) => {
-    return res.status(200).json({
-        success: true,
-        user: req.user
-    });
+  return res.status(200).json({
+    success: true,
+    user: req.user
+  });
 };
 
 const sendToken = (user, statusCode, res) => {
-    const token = user.getSignedToken();
+  const token = user.getSignedToken();
 
-    res.status(statusCode).json({
-        success: true,
-        token: token
-    });
+  res.status(statusCode).json({
+    success: true,
+    token: token
+  });
 };
